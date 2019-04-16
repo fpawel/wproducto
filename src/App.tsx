bf1d6ea08762e26b234c0b05e3f3139cc3787267
binf1611@gmail.com <fpawel>
2019-04-16 23:14:26 +0300

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,109 @@
 import React, { Component } from "react";
 import {
-  BrowserRouter as Router,
-  Route, Switch,
-  Link,
-  RouteComponentProps,
-  match
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Link,
+    RouteComponentProps,
+    match
 } from "react-router-dom";
 import "./App.css";
 import Login from "./Login";
 import Register from "./Register";
+import Profile from "./Profile";
+import {Container, Jumbotron} from "react-bootstrap";
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/topics" component={Topics} />
-          <Route component={NoMatch} />
-        </Switch>
-
-
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div>
+                <Header />
+                <Switch>
+                    <Route exact path="/" component={Welcome} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                    <Route path="/profile" component={Profile} />
+                    <Route component={NoMatch} />
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
-function Home() {
-  return <h2>Home</h2>;
+function Welcome(props: any) {
+    return (
+        <Jumbotron>
+            <Container>
+                <h2>Здравствуйте</h2>
+                <p>Добро пожаловать на наш сайт.</p>
+                <p>
+                    Пожалуйста, <Link to="/register">зарегистрируйтесь</Link>, чтобы регулярно получать скидки и бонусы
+                    на депозит, а так же принять участие в наших розsгрышах.
+                </p>
+                <p>
+                    Зарегестрированы? Проверьте свой <Link to="/profile">личный кабинет</Link>.
+                </p>
+            </Container>
+
+        </Jumbotron>
+    );
 }
 
-function Topic({ match }: RouteComponentProps<{ id: string }>) {
-  return <h3>Requested Param: {match.params.id}</h3>;
-}
+// function Topic({ match }: RouteComponentProps<{ id: string }>) {
+//   return <h3>Requested Param: {match.params.id}</h3>;
+// }
 
-function Topics({ match }: RouteComponentProps<{ id: string }>) {
-  return (
-    <div>
-      <h2>Topics</h2>
+// function Topics({ match }: RouteComponentProps<{ id: string }>) {
+//   return (
+//     <div>
+//       <h2>Topics</h2>
 
-      <ul>
-        <li>
-          <Link to={`${match.url}/components`}>Components</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-        </li>
-      </ul>
+//       <ul>
+//         <li>
+//           <Link to={`${match.url}/components`}>Components</Link>
+//         </li>
+//         <li>
+//           <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+//         </li>
+//       </ul>
 
-      <Route path={`${match.path}/:id`} component={Topic} />
-      <Route
-        exact
-        path={match.path}
-        render={() => <h3>Please select a topic.</h3>}
-      />
-    </div>
-  );
-}
+//       <Route path={`${match.path}/:id`} component={Topic} />
+//       <Route
+//         exact
+//         path={match.path}
+//         render={() => <h3>Please select a topic.</h3>}
+//       />
+//     </div>
+//   );
+// }
 
 function NoMatch(props: any) {
-  return (
-    <div>
-      <h3>
-        No match for <code>{props.location.pathname}</code>
-      </h3>
-    </div>
-  );
+    return (
+        <div>
+            <h3>
+                No match for <code>{props.location.pathname}</code>
+            </h3>
+        </div>
+    );
 }
 
 function Header() {
-  return (
-    <ul>
-      <li>
-        <Link to="/">Домой</Link>
-      </li>
-      <li>
-        <Link to="/login">Вход</Link>
-      </li>
-      <li>
-        <Link to="/register">Регистрация</Link>
-      </li>
-      <li>
-        <Link to="/topics">Topics</Link>
-      </li>
-    </ul>
-  );
+    return (
+        <ul>
+            <li>
+                <Link to="/">Добро пожаловать</Link>
+            </li>
+            <li>
+                <Link to="/login">Вход</Link>
+            </li>
+            <li>
+                <Link to="/register">Регистрация</Link>
+            </li>
+            <li>
+                <Link to="/profile">Личный кабинет</Link>
+            </li>
+        </ul>
+    );
 }
 
 export default App;
--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import * as AppKey from "./AppKey";
 import {
-    Button, Form, Spinner, Row, Alert, Card, FormGroup, FormLabel, FormControl
+    Button, Form, Spinner, Row, Alert, Card, FormGroup, FormLabel, FormControl, Jumbotron, Container
 } from 'react-bootstrap';
 import { jsonrpc2 } from "./Api"
 import { Link, Redirect } from "react-router-dom";
@@ -9,172 +9,89 @@ import { Link, Redirect } from "react-router-dom";
 
 
 interface State {
-    name: string;
-    email: string;
-    pass: string;
-    passAgain: string;
-    request: boolean;
-    error?: string
-    redirectTo?: string,
+    payload: {
+        kind: "initialized";
+        name: string;
+        email: string;
+    } | {
+        kind: "initializing";
+    } | {
+        kind: "error";
+        error: string;
+    }
 }
 
 export default class Profile extends React.Component<{}, State> {
     constructor(props: {}) {
         super(props);
         this.state = {
-            name: "",
-            email: "",
-            pass: "",
-            error: "",
-            passAgain: "",
-            request: false,
+            payload: {
+                kind: "initializing",
+            },
         };
-        this.handleInputName = this.handleInputName.bind(this);
-        this.handleInputPassword = this.handleInputPassword.bind(this);
-        this.handleInputPasswordAgain = this.handleInputPasswordAgain.bind(this);
-        this.handleInputEmail = this.handleInputEmail.bind(this);
     }
 
-
-    handleInputName(evt: any) {
-        this.setState({ name: evt.target.value });
-    }
-
-    handleInputEmail(evt: any) {
-        this.setState({ email: evt.target.value });
-    }
-
-    handleInputPassword(evt: any) {
-        this.setState({ pass: evt.target.value });
-    }
-    handleInputPasswordAgain(evt: any) {
-        this.setState({ passAgain: evt.target.value });
-    }
-
-    setError(error: string) {
-        this.setState({
-            request: false,
-            error: error,
-        });
-    }
-
-    async handleClick() {
-        this.setState({ request: true });
-
+    async componentDidMount() {
         try {
-            let response = await jsonrpc2("Auth.Register", {
-                name: this.state.name,
-                pass: this.state.pass,
-                email: this.state.email,
-                role: "regular_user",
-            });
+            let response = await jsonrpc2("Auth.Profile", [localStorage.getItem(AppKey.token)]);
             if (response.kind === "ok") {
-                localStorage.setItem(AppKey.token, response.result);
                 this.setState({
-                    redirectTo: "/profile",
+                    payload: {
+                        kind: "initialized",
+                        name: response.result.Name,
+                        email: response.result.Email,
+                    },
                 });
                 return;
             }
-            this.setError(response.error.message);
+            this.setState({
+                payload: {
+                    kind: "error",
+                    error: response.error.message,
+                },
+            });
         } catch (exn) {
-            this.setError("Что-то пошло не так. Подробности в консоли браузера.");
+            this.setState({
+                payload: {
+                    kind: "error",
+                    error: "Подробности в консоли браузера.",
+                },
+            });
         }
+
     }
+
 
     render() {
-        if (this.state.redirectTo) {
-            return <Redirect to={this.state.redirectTo} />
+        if (this.state.payload.kind === "initializing") {
+            return (<main>
+                <Spinner animation="border" />
+            </main>);
         }
 
-        let { name, email, pass, passAgain, error } = this.state;
-
-        return (
-
-            <main >
-                <div>
-
-                    <Form style={{
-                        margin: "0 auto",
-                        width: "50%",
-                        maxWidth: "400px",
-                        border: "3px solid lightsteelblue",
-                        background: "lightcyan",
-                        borderRadius: "15px",
-                        padding: "40px 40px 20px 40px",
-                    }}>
-
-                        <Form.Group as={Row} >
-                            <FormLabel>Имя пользователя</FormLabel>
-                            <Form.Control type="name" placeholder="Имя пользователя"
-                                required onChange={this.handleInputName}
-                                value={name}
-                            />
-                        </Form.Group>
-
-                        <Form.Group as={Row} >
-                            <FormLabel>Адрес электронной почты</FormLabel>
-                            <Form.Control type="email" placeholder="Имя пользователя или email"
-                                required onChange={this.handleInputEmail}
-                                value={email} />
-                        </Form.Group>
-
-                        <Form.Group as={Row} >
-                            <FormLabel>Пароль</FormLabel>
-                            <Form.Control type="password" placeholder="Пароль"
-                                value={pass}
-                                required onChange={this.handleInputPassword} />
-                        </Form.Group>
-
-                        <Form.Group as={Row} >
-                            <FormLabel>Пароль ещё раз</FormLabel>
-                            <Form.Control type="password" placeholder="Пароль ещё раз"
-                                isValid={passAgain.length > 0 && pass === passAgain}
-                                isInvalid={passAgain.length > 0 && pass !== passAgain}
-                                value={passAgain}
-                                required onChange={this.handleInputPasswordAgain} />
-
-                            {pass !== passAgain ?
-                                <FormControl.Feedback type="valid">
-                                    повторно введённый пароль не совпадает с введённым первоначально"
-                                </FormControl.Feedback>
-                                : null
-                            }
-                        </Form.Group>
+        if (this.state.payload.kind === "error") {
+            return (<main>
+                <Alert variant='danger' style={{ marginTop: "20px" }}>
+                    <h2>Что-то пошло не так.</h2>
+                    <p>{this.state.payload.error}</p>
+                    <p>Попробуйте <Link to="/profile" > войти в личный кабинет </Link>  ещё раз.
+                    </p>
+                </Alert>
+            </main>);
+        }
 
 
-                        <Button variant="primary" type="submit"
-                            onClick={this.handleClick}
-                            className="float-right"
-                            disabled={this.state.request || pass !== passAgain}>
-                            Регистрация
-                            {this.state.request ?
-                                <Spinner
-                                    as="span"
-                                    animation="grow"
-                                    size="sm"
-                                    role="status"
-                                    aria-hidden="true"
-                                /> : null}
-                        </Button>
-                        <Link to="/profile" >
-                            Уже зарегестрированы?
-                        </Link>
+        return (<main>
+            <Jumbotron>
+                <Container>
+                    <h2>Личный кабинет</h2>
+                    <h3>{this.state.payload.name}</h3>
+                    <h4>{this.state.payload.email}</h4>
+                </Container>
 
-                        {!this.state.request && error ?
-                            (
-                                <Alert variant='danger' style={{ marginTop: "20px" }}>
-                                    {error}
-                                </Alert>
-                            ) : null
-                        }
-                    </Form>
-                </div>
-            </main>
-        );
+            </Jumbotron>
+
+        </main>);
     }
 }
 
-function validateEmail(email: string) {
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-}
